a7f301a55ad4c1ee61b8dc96116352f73b5dac5b
dibenedettoivan <dibenedettoivan@gmail.com>
2015-06-04 14:56:25 +0200

    Modifica

--- src/CercaNumero.java
+++ src/CercaNumero.java
@@ -5,16 +5,17 @@ public class CercaNumero {
 		
 		int maxNum = 10000;
 		int numeroCasuale;
-		int bersaglio = 10000;
+		int bers = 10000;
 		int tentativi = 0;
+		
 		while(true){
 			numeroCasuale = (int)(Math.random() * maxNum + 1);
 			tentativi++;
-			if (numeroCasuale == bersaglio) 
+			if (numeroCasuale == bers) 
 				break;
 			
 			
-		}	System.out.println("Per trovare il numero " + bersaglio + " sono stati fatti " + tentativi + " tentativi.");
+		}	System.out.println("Per trovare il numero " + bers + " sono stati fatti " + tentativi + " tentativi.");
 	}
 
 }
